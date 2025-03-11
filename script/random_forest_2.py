92c5ec7db112f4353175aa54869b9848f89236bf
Péter Rácz <petermartonracz@gmail.com>
2025-03-11 09:03:24 +0100

    rf

--- script/random_forest_2.py
+++ script/random_forest_2.py
@@ -11,7 +11,10 @@ from sklearn.metrics import confusion_matrix
 
 # load data
 
-data = pd.read_csv('https://raw.githubusercontent.com/petyaracz/GTK/refs/heads/main/dat/accident.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/petyaracz/GTK/refs/heads/main/dat/credit_risk_dataset.csv')
+
+# sample data down to 30%
+data = data.sample(frac=0.3, random_state=42)
 
 # nrow data
 print(data.shape)
@@ -25,34 +28,35 @@ data = pd.get_dummies(data, drop_first=True)
 data = data.dropna()
 
 # split data into features and target
-X = data.drop("Survived", axis=1)
-y = data["Survived"]
+X = data.drop("loan_status", axis=1)
+y = data["loan_status"]
 
-# summary of y
-y.value_counts()
+# training test split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # create random forest classifier
 clf = RandomForestClassifier()
 
 # create parameter grid
 param_grid = {
-    'n_estimators': [100, 500],
+    'n_estimators': [500, 700],
     # 'max_depth': [None, 10, 20, 30, 40, 50],
-    'min_samples_split': [1, 2, 5],
-    'min_samples_leaf': [1, 2],
+    'min_samples_split': [5, 10],
+    'min_samples_leaf': [5, 10],
     # 'max_features': ['auto', 'sqrt', 'log2', None],
-    'max_leaf_nodes': [None, 10],
-    # 'min_impurity_decrease': [0.0, 0.01, 0.05, 0.1],
+    'max_leaf_nodes': [None, 3, 5, 10],
+    'min_impurity_decrease': [0.0, 0.01, 0.05, 0.1],
     # 'class_weight': [None, 'balanced', 'balanced_subsample']
 }
 
 # create grid searches
 
-## using accuracy and random search
-grid_search = RandomizedSearchCV(estimator=clf, param_distributions=param_grid, n_iter=5000, cv=3, n_jobs=-1, random_state=42, scoring='accuracy', verbose=2)
+## using accuracy and grid search
+grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=3, n_jobs=-1, scoring='accuracy', verbose=2)
 
 # fit grid search
-grid_search.fit(X, y)
+grid_search.fit(X_train, y_train)
 
 # get confusion matrix on training data from best model
 best_model = grid_search.best_estimator_
@@ -62,3 +66,10 @@ confusion_matrix(y, y_pred)
 accuracy_score(y, y_pred)
 # get model attr
 best_model.get_params()
+
+# get confusion matrix on test data from best model
+y_pred = best_model.predict(X_test)
+confusion_matrix(y_test, y_pred)
+# get accuracy
+accuracy_score(y_test, y_pred)
+# nice
