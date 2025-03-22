f0b1ff4b28ad99bd9850356295ed31061c2b3a71
Péter Rácz <petermartonracz@gmail.com>
2025-03-22 15:10:50 +0100

    random forest ora

--- script/random_forest.py
+++ script/random_forest.py
@@ -1,64 +1,117 @@
+############
+# setup
+############
+
+from ucimlrepo import fetch_ucirepo 
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+# load sklearn dt
+from sklearn.tree import DecisionTreeClassifier
 # load sklearn random forest classifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+# acc score roc auc
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 from sklearn.model_selection import GridSearchCV
-# load RandomizedSearchCV
-# from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import confusion_matrix
+from sklearn.tree import plot_tree
+  
+############
+# dataset
+############
+  
+# fetch dataset 
+wine = fetch_ucirepo(id=109) 
+  
+# data (as pandas dataframes) 
+X = wine.data.features 
+y = wine.data.targets.values.ravel() # to flatten array
+  
+# heatmap of X
+plt.figure(figsize=(10, 10))
+sns.heatmap(X.corr(), annot=True, cmap='coolwarm')
+plt.title('Correlation Heatmap')
+plt.show()
 
-# load data
+# count values of y
+print(y.value_counts())
+X.dtypes
 
-data = pd.read_csv('https://raw.githubusercontent.com/petyaracz/GTK/refs/heads/main/dat/accident.csv')
+# split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# nrow data
-print(data.shape)
+############
+# models
+############
 
-data.dtypes
+# Decision Tree
+dt = DecisionTreeClassifier()
 
-# if column object dummy code column
-data = pd.get_dummies(data, drop_first=True)
-
-# drop rows with missing values
-data = data.dropna()
-
-# split data into features and target
-X = data.drop("Survived", axis=1)
-y = data["Survived"]
-
-# summary of y
-y.value_counts()
-
-# create random forest classifier
-clf = RandomForestClassifier()
-
-# create parameter grid
-param_grid = {
-    'n_estimators': [100, 500],
-    # 'max_depth': [None, 10, 20, 30, 40, 50],
-    'min_samples_split': [1, 2, 5],
-    'min_samples_leaf': [1, 2],
-    # 'max_features': ['auto', 'sqrt', 'log2', None],
-    'max_leaf_nodes': [None, 10],
-    # 'min_impurity_decrease': [0.0, 0.01, 0.05, 0.1],
-    # 'class_weight': [None, 'balanced', 'balanced_subsample']
+# grid for DT
+param_grid_dt = {
+    'max_depth': [10, 20, 30, 40, 50],
+    'min_samples_split': [2, 5, 10, 15, 20],
+    'min_samples_leaf': [1, 2, 5, 10]
 }
 
-# create grid searches
+# grid search
+grid_dt = GridSearchCV(dt, param_grid_dt, cv=5)
 
-## using accuracy and random search
-grid_search = RandomizedSearchCV(estimator=clf, param_distributions=param_grid, n_iter=5000, cv=3, n_jobs=-1, random_state=42, scoring='accuracy', verbose=2)
+# rf
+rf = RandomForestClassifier(n_estimators=100)
 
-# fit grid search
-grid_search.fit(X, y)
+# grid for RF
+param_grid_rf = {
+    'max_features': [None, 'sqrt', 'log2'],
+    'max_depth' : [5, 50],
+    'min_samples_split': [2, 10],
+    'min_samples_leaf': [2, 10],
+    'max_samples': [0.33, 0.66, 1]  # Add this line to include max_samples
+}
 
-# get confusion matrix on training data from best model
-best_model = grid_search.best_estimator_
-y_pred = best_model.predict(X)
-confusion_matrix(y, y_pred)
-# get accuracy
-accuracy_score(y, y_pred)
-# get model attr
-best_model.get_params()
+# grid search
+grid_rf = GridSearchCV(rf, param_grid_rf, cv=5)
+
+############
+# fit
+############
+
+grid_dt.fit(X_train, y_train)
+# best pars
+grid_dt.best_params_
+grid_rf.fit(X_train, y_train)
+grid_rf.best_params_
+
+############
+# predict
+############
+
+y_pred_dt = grid_dt.predict(X_test)
+y_pred_rf = grid_rf.predict(X_test)
+
+# accuracy
+accuracy_score(y_test, y_pred_dt)
+accuracy_score(y_test, y_pred_rf)
+
+# confusion matrix
+confusion_matrix(y_test, y_pred_dt)
+confusion_matrix(y_test, y_pred_rf)
+
+# best params
+print(grid_dt.best_params_)
+print(grid_rf.best_params_)
+
+# plot dt tree
+
+plt.figure(figsize=(20, 20))    
+plot_tree(grid_dt.best_estimator_, filled=True)
+plt.show()
+
+# plot best rf tree
+plt.figure(figsize=(20, 20))
+plot_tree(grid_rf.best_estimator_[0], filled=True)
+plt.show()
--- script/random_forest_2.py
+++ script/random_forest_2.py
@@ -1,75 +1,41 @@
+############
+# setup
+############
+
+from ucimlrepo import fetch_ucirepo 
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+# load sklearn dt
+from sklearn.tree import DecisionTreeClassifier
 # load sklearn random forest classifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+# acc score roc auc
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 from sklearn.model_selection import GridSearchCV
-# load RandomizedSearchCV
-# from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import confusion_matrix
+from sklearn.tree import plot_tree
+  
+############
+# dataset
+############
+  
+# fetch dataset 
+heart_disease = fetch_ucirepo(id=45) 
+  
+# data (as pandas dataframes) 
+X = heart_disease.data.features 
+y = heart_disease.data.targets 
+  
+# metadata 
+print(heart_disease.metadata) 
+  
+# variable information 
+print(heart_disease.variables) 
 
-# load data
-
-data = pd.read_csv('https://raw.githubusercontent.com/petyaracz/GTK/refs/heads/main/dat/credit_risk_dataset.csv')
-
-# sample data down to 30%
-data = data.sample(frac=0.3, random_state=42)
-
-# nrow data
-print(data.shape)
-
-data.dtypes
-
-# if column object dummy code column
-data = pd.get_dummies(data, drop_first=True)
-
-# drop rows with missing values
-data = data.dropna()
-
-# split data into features and target
-X = data.drop("loan_status", axis=1)
-y = data["loan_status"]
-
-# training test split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# create random forest classifier
-clf = RandomForestClassifier()
-
-# create parameter grid
-param_grid = {
-    'n_estimators': [500, 700],
-    # 'max_depth': [None, 10, 20, 30, 40, 50],
-    'min_samples_split': [5, 10],
-    'min_samples_leaf': [5, 10],
-    # 'max_features': ['auto', 'sqrt', 'log2', None],
-    'max_leaf_nodes': [None, 3, 5, 10],
-    'min_impurity_decrease': [0.0, 0.01, 0.05, 0.1],
-    # 'class_weight': [None, 'balanced', 'balanced_subsample']
-}
-
-# create grid searches
-
-## using accuracy and grid search
-grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=3, n_jobs=-1, scoring='accuracy', verbose=2)
-
-# fit grid search
-grid_search.fit(X_train, y_train)
-
-# get confusion matrix on training data from best model
-best_model = grid_search.best_estimator_
-y_pred = best_model.predict(X)
-confusion_matrix(y, y_pred)
-# get accuracy
-accuracy_score(y, y_pred)
-# get model attr
-best_model.get_params()
-
-# get confusion matrix on test data from best model
-y_pred = best_model.predict(X_test)
-confusion_matrix(y_test, y_pred)
-# get accuracy
-accuracy_score(y_test, y_pred)
-# nice
+# X nrows
+print(X.shape[0])
